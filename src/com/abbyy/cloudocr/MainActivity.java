63392e2e4cb304983c905aadd751926a3a051f55
Denis Lapuente <lapuente@abbyy.com>
2013-01-08 22:17:02 +0100

    CHANGE ALL

--- src/com/abbyy/cloudocr/MainActivity.java
+++ src/com/abbyy/cloudocr/MainActivity.java
@@ -2,12 +2,23 @@ package com.abbyy.cloudocr;
 
 import android.os.Bundle;
 
-public class MainActivity extends ActionBarActivity{
+import com.abbyy.cloudocr.compat.TabCompatActivity;
+import com.abbyy.cloudocr.compat.TabHelper;
 
+public class MainActivity extends TabCompatActivity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		setTabs();
+		super.onCreate(savedInstanceState);
+	}
+	
+	private void setTabs(){
+		TabHelper tabHelper = getTabHelper();
+		tabHelper.addTab(tabHelper.newTab(getString(R.string.tab_active_tasks)));
+		tabHelper.addTab(tabHelper.newTab(getString(R.string.tab_completed_tasks)));
 	}
 
 }
--- src/com/abbyy/cloudocr/optionsfragments/ProcessOptionsFragment.java
+++ src/com/abbyy/cloudocr/optionsfragments/ProcessOptionsFragment.java
@@ -1,11 +1,59 @@
 package com.abbyy.cloudocr.optionsfragments;
 
+import java.io.InputStream;
+
+import com.abbyy.cloudocr.AsyncInputStreamLoader;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
 
 public abstract class ProcessOptionsFragment extends Fragment {
 	protected String mTaskId;
+	private static final int LOADER_LAUNCH_TASK = 1;
+
 	public abstract String createURL();
 	public abstract Bundle createArgs();
 	public abstract boolean setDefaultOptions(Bundle options);
+
+	public void launchTask() {
+		this.createURL();
+
+		getActivity().getSupportLoaderManager().initLoader(LOADER_LAUNCH_TASK,
+				createArgs(), new ConnectionHelper());
+	}
+	
+	private class ConnectionHelper implements LoaderCallbacks<InputStream>{
+
+		@Override
+		public Loader<InputStream> onCreateLoader(int id, Bundle args) {
+			Loader<InputStream> loader = null;
+			switch(id){
+			case LOADER_LAUNCH_TASK:
+				loader = new AsyncInputStreamLoader(getActivity(), args);
+				break;
+			}
+			return loader;
+		}
+
+		@Override
+		public void onLoadFinished(Loader<InputStream> loader, InputStream result) {
+			switch(loader.getId()){
+			case LOADER_LAUNCH_TASK:
+				parseData(result);
+				break;
+			}
+			loader.abandon();
+		}
+
+		@Override
+		public void onLoaderReset(Loader<InputStream> loader) {
+			loader.reset();
+		}
+	}
+	
+	private void parseData(InputStream stream){
+		
+	}
 }
